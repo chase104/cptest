1e0d55e8efc8531cdc46ec60bf0608a551f348f2
chase104 <62479547+chase104@users.noreply.github.com>
2024-07-17 17:08:07 -0500

    8

---
+++ index8.sql
@@ -0,0 +1,20 @@
+-- write some random sql
+
+SELECT * FROM table_name;
+
+SELECT * FROM table_n;
+
+-- anotherm iddle
+
+select * from middlechanges;
+asdfd
+
+
+select * from me;
+
+-- 6
+
+-- endd
+
+
+-- 8 changes
