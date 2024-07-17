7276e9df459770c390ad4084573b5a6c701b3a09
chase104 <62479547+chase104@users.noreply.github.com>
2024-07-17 17:10:20 -0500

    Renamed index7.sql to index10.sql and copied index10.sql back to index7.sql

---
+++ index10.sql
@@ -0,0 +1,17 @@
+-- write some random sql
+
+SELECT * FROM table_name;
+
+SELECT * FROM table_n;
+
+-- anotherm iddle
+
+select * from middlechanges;
+asdfd
+
+
+select * from me;
+
+-- 6
+
+-- endd
