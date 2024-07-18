d5b8e791ecfa91042cb0a83242dc0406c74aaca2
chase104 <62479547+chase104@users.noreply.github.com>
2024-07-18 10:39:16 -0500

    Moved index10.sql to 28.sql and copied it to _index10.sql

---
+++ _index10.sql
@@ -0,0 +1,19 @@
+-- write some random sql
+
+SELECT * FROM table_name;
+
+SELECT * FROM table_n;
+
+-- anotherm iddle
+
+select * from middlechanges;
+asdfd
+
+
+select * from me;
+
+-- 6
+
+-- endd
+
+-- added 10
