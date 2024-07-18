8fc480175581b62dd5636b1d2b4c96f8a7f8766c
chase104 <62479547+chase104@users.noreply.github.com>
2024-07-18 10:12:37 -0500

    made _25 and modified 26 new

---
+++ 26.sql
@@ -0,0 +1,23 @@
+-- write some random sql
+
+SELECT * FROM table_name;
+
+SELECT * FROM table_n;
+
+-- anotherm iddle
+
+select * from middlechanges;
+asdfd
+
+
+select * from me;
+
+-- 6
+
+-- endd
+
+-- added 10
+
+--added 25
+
+-- made changes of 26
