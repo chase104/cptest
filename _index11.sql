a86ed401502cb3f010c0ad7e46c0d635eb76d3d7
chase104 <62479547+chase104@users.noreply.github.com>
2024-07-18 10:44:39 -0500

    Moved index11.sql to 29.sql and copied it to _index11.sql

---
+++ _index11.sql
@@ -0,0 +1,21 @@
+-- write some random sql
+
+SELECT * FROM table_name;
+
+SELECT * FROM table_n;
+
+-- anotherm iddle
+
+select * from middlechanges;
+asdfd
+
+-- 11 chnage
+
+
+select * from me;
+
+-- 6
+
+-- endd
+
+-- 7 changes
