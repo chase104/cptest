85f05c83d1c861ba6b3fdba31d2cd201596bdcda
chase104 <62479547+chase104@users.noreply.github.com>
2024-07-18 10:10:54 -0500

    added some to 25

---
+++ 25.sql
@@ -0,0 +1,21 @@
+-- write some random sql
+
+SELECT * FROM table_name;
+
+SELECT * FROM table_n;
+
+-- anotherm iddle
+
+select * from middlechanges;
+asdfd
+
+
+select * from me;
+
+-- 6
+
+-- endd
+
+-- added 10
+
+--added 25
--- scr.sh
+++ scr.sh
@@ -1,60 +1,20 @@
-#!/bin/sh
-
-# Check if correct number of arguments is provided
+# Check if the correct number of arguments is provided
 if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <old_filename> <new_filename>"
-  exit 1
+    echo "Usage: $0 <old_filename> <new_filename>"
+    exit 1
 fi
 
-OLD_FILE=$1
-NEW_FILE=$2
-TEMP_FILE="temp.sql"
-
-# Check if the old file exists
-if [ ! -f "$OLD_FILE" ]; then
-  echo "File $OLD_FILE does not exist."
-  exit 1
-fi
-
-# Copy the old file to a temporary file
-cp "$OLD_FILE" "$TEMP_FILE"
-if [ $? -ne 0 ]; then
-  echo "Failed to copy $OLD_FILE to $TEMP_FILE"
-  exit 1
-fi
+# Assign arguments to variables
+OLD_FILENAME=$1
+NEW_FILENAME=$2
 
 # Move the old file to the new file
-git mv "$OLD_FILE" "$NEW_FILE"
-if [ $? -ne 0 ]; then
-  echo "Failed to move $OLD_FILE to $NEW_FILE"
-  exit 1
-fi
+git mv "$OLD_FILENAME" "$NEW_FILENAME"
 
-# Commit the move
-git commit -m "Renamed $OLD_FILE to $NEW_FILE"
-if [ $? -ne 0 ]; then
-  echo "Failed to commit the rename"
-  exit 1
-fi
+ cp "$NEW_FILENAME" "_$OLD_FILENAME"
 
-# Move the temporary file back to the old file name
-# mv "$TEMP_FILE" "$OLD_FILE"
-# if [ $? -ne 0 ]; then
-#   echo "Failed to move $TEMP_FILE to $OLD_FILE"
-#   exit 1
-# fi
+# Copy the new file back to the old filename
+# Add both files to the staging area
 
-# Add and commit the new file with the old name
-git add "$TEMP_FILE"
-if [ $? -ne 0 ]; then
-  echo "Failed to add the new $OLD_FILE"
-  exit 1
-fi
 
-git commit -m "Added new $OLD_FILE with the same contents as $NEW_FILE"
-if [ $? -ne 0 ]; then
-  echo "Failed to commit the new $OLD_FILE"
-  exit 1
-fi
-
-echo "Operation completed successfully."
+echo "Files have been moved and copied successfully. Please review the changes before pushing."
